99fcf69a421baf11960b10cd7ff6e104aa4e89d5
Ivan Gozhyi <givan2981@gmail.com>
2026-06-23 19:05:45 +0300

    done login and register with AWS

--- auth.ts
+++ auth.ts
@@ -8,10 +8,11 @@ import { PrismaPg } from "@prisma/adapter-pg"
 const pool = new Pool({ connectionString: process.env.DATABASE_URL })
 const adapter = new PrismaPg(pool)
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({ adapter });
 
 export const {handlers, auth, signIn, signOut} = NextAuth({
     adapter: PrismaAdapter(prisma),
+    secret: process.env.AUTH_SECRET,
     providers: [
         Cognito({
             clientId: process.env.COGNITO_CLIENT_ID!,
